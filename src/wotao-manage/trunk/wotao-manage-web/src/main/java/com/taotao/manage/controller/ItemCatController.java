9d1d4e12e78debaad8da121f8df563b50e4fcf5b
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-05 07:19:43 +0000

    day01根据父类id查询类目集合，仅一个方法，给service传的父类id是0L public ResponseEntity<List<ItemCat>> queryItemCatListByParentId(
    git-svn-id: svn://localhost/23taotao@19 282828a6-f508-ba43-874f-5e0e018e640a

---
+++ src/wotao-manage/trunk/wotao-manage-web/src/main/java/com/taotao/manage/controller/ItemCatController.java
@@ -0,0 +1,39 @@
+package com.taotao.manage.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.taotao.manage.pojo.ItemCat;
+import com.taotao.manage.service.ItemCatService;
+
+
+@RequestMapping("item/cat")
+@Controller
+public class ItemCatController {
+
+    @Autowired
+    private ItemCatService itemCatService;
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<ItemCat>> queryItemCatListByParentId(
+            @RequestParam(value = "id", defaultValue = "0") Long parentId) {
+        try {
+            List<ItemCat> list = this.itemCatService.queryItemCatByParentId(0L);
+            if (null == list || list.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+            return ResponseEntity.ok(list);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+}
