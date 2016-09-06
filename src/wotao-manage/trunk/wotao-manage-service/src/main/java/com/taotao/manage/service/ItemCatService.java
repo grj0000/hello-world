a59438a11f86bba1f32ad67cbe933f9e5b717007
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-06 01:37:39 +0000

    day02 因为BaseService没有了抽象方法，所以做了修改 //    @Override //    public Mapper<ItemCat> getMapper() { //        return this.itemCatMapper;
    git-svn-id: svn://localhost/23taotao@30 282828a6-f508-ba43-874f-5e0e018e640a

--- src/wotao-manage/trunk/wotao-manage-service/src/main/java/com/taotao/manage/service/ItemCatService.java
+++ src/wotao-manage/trunk/wotao-manage-service/src/main/java/com/taotao/manage/service/ItemCatService.java
@@ -1,7 +1,5 @@
 package com.taotao.manage.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,19 +7,25 @@ import com.taotao.manage.mapper.ItemCatMapper;
 import com.taotao.manage.pojo.ItemCat;
 
 @Service
-public class ItemCatService {
+public class ItemCatService extends BaseService<ItemCat> {
 
     @Autowired
     private ItemCatMapper itemCatMapper;
-	/**
-	 * 全部查询，并且生成树状结构
-	 * 
-	 * @return
-	 */
-	public List< ItemCat> queryItemCatByParentId(Long parentId) {
 
-		ItemCat record = new ItemCat();
-		record.setParentId(parentId);
-        return this.itemCatMapper.select(record );
-	}
+    /**
+     * 全部查询，并且生成树状结构
+     * 
+     * @return
+     */
+//    public List<ItemCat> queryItemCatByParentId(Long parentId) {
+//
+//        ItemCat record = new ItemCat();
+//        record.setParentId(parentId);
+//        return this.itemCatMapper.select(record);
+//    }
+
+//    @Override
+//    public Mapper<ItemCat> getMapper() {
+//        return this.itemCatMapper;
+//    }
 }
