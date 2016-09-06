9fe8a977fcac0ebee533748d67a4c1f24582947a
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-06 08:53:56 +0000

    day02 添加日志 if(LOGGER.isInfoEnabled()){                 LOGGER.info("新增商品, item={}, desc={}", item, desc);             } } catch (Exception e) {             LOGGER.error("新增商品失败，title= " + item.getTitle() + " , cid=" + item.getCid(), e);         }
    git-svn-id: svn://localhost/23taotao@57 282828a6-f508-ba43-874f-5e0e018e640a

--- src/wotao-manage/trunk/wotao-manage-web/src/main/java/com/taotao/manage/controller/ItemController.java
+++ src/wotao-manage/trunk/wotao-manage-web/src/main/java/com/taotao/manage/controller/ItemController.java
@@ -1,6 +1,8 @@
 package com.taotao.manage.controller;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +14,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.github.pagehelper.PageInfo;
 import com.taotao.common.bean.EasyUIResult;
 import com.taotao.manage.pojo.Item;
-import com.taotao.manage.pojo.ItemDesc;
-import com.taotao.manage.service.ItemDescService;
 import com.taotao.manage.service.ItemService;
 
 @Controller
 @RequestMapping("item")
 public class ItemController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ItemCatController.class);
 
     @Autowired
     private ItemService itemService;
@@ -26,6 +28,10 @@ public class ItemController {
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> saveItem(Item item, @RequestParam("desc") String desc) {
         try {
+            if(LOGGER.isInfoEnabled()){
+                LOGGER.info("新增商品, item={}, desc={}", item, desc);
+            }
+            
             if (StringUtils.isEmpty(item.getTitle())) {
                 // 响应400
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
@@ -33,11 +39,14 @@ public class ItemController {
 
             // 保存商品的基本数据
             this.itemService.save(item, desc);
-
+            if(LOGGER.isInfoEnabled()){
+                LOGGER.info("新增商品成功, itemId={}", item.getId());
+            }
+            
             // 成功 201
             return ResponseEntity.status(HttpStatus.CREATED).build();
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.error("新增商品失败，title= " + item.getTitle() + " , cid=" + item.getCid(), e);
         }
         // 出错 500
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
@@ -48,17 +57,17 @@ public class ItemController {
             @RequestParam(value = "page", defaultValue = "1") Integer page,
             @RequestParam(value = "rows", defaultValue = "30") Integer rows) {
         try {
-            if (page<1||rows<1) {
+            if (page < 1 || rows < 1) {
                 // 响应400
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
             }
 
             // 保存商品的基本数据
-            PageInfo<Item> pageInfo = this.itemService.queryItemList(page,rows);
+            PageInfo<Item> pageInfo = this.itemService.queryItemList(page, rows);
 
             EasyUIResult easyUIResult = new EasyUIResult(pageInfo.getTotal(), pageInfo.getList());
             // 成功 200
-            return ResponseEntity.status(HttpStatus.OK).body(easyUIResult );
+            return ResponseEntity.status(HttpStatus.OK).body(easyUIResult);
         } catch (Exception e) {
             e.printStackTrace();
         }
