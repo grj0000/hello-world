023617f0ff099c75b04b88c2b90f2808cd6bc1aa
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-06 03:32:36 +0000

    day02 放在一个事务中  @Autowired     private ItemDescService itemDescService;     public void save(Item item, String desc) {
    git-svn-id: svn://localhost/23taotao@43 282828a6-f508-ba43-874f-5e0e018e640a

--- src/wotao-manage/trunk/wotao-manage-service/src/main/java/com/taotao/manage/service/ItemService.java
+++ src/wotao-manage/trunk/wotao-manage-service/src/main/java/com/taotao/manage/service/ItemService.java
@@ -1,10 +1,30 @@
 package com.taotao.manage.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.taotao.manage.pojo.Item;
+import com.taotao.manage.pojo.ItemDesc;
 
 @Service
 public class ItemService extends BaseService<Item>{
+
+    @Autowired
+    private ItemDescService itemDescService;
+    
+    public void save(Item item, String desc) {
+
+        // 设置初始数据
+        item.setStatus(1);
+        item.setId(null);// 强制设置id为null
+        
+        this.save(item);
+        
+        ItemDesc itemDesc = new ItemDesc();
+        itemDesc.setItemId(item.getId());
+        itemDesc.setItemDesc(desc);
+        // 保存描述数据
+        this.itemDescService.save(itemDesc);
+    }
 
 }
