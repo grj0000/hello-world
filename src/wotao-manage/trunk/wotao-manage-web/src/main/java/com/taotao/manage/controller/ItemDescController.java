e2199dbbe0df93513d2e326c8359af0e620ecf12
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-07 10:18:45 +0000

    day03 用于商品描述的回显  public ResponseEntity<ItemDesc> queryItemDescByItemId(@PathVariable("itemId")Long itemId) {     ItemDesc itemDesc = this.itemDescService.queryById(itemId);
    git-svn-id: svn://localhost/23taotao@75 282828a6-f508-ba43-874f-5e0e018e640a

---
+++ src/wotao-manage/trunk/wotao-manage-web/src/main/java/com/taotao/manage/controller/ItemDescController.java
@@ -0,0 +1,45 @@
+package com.taotao.manage.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.taotao.manage.pojo.ItemDesc;
+import com.taotao.manage.service.ItemDescService;
+
+
+@Controller
+@RequestMapping("item/desc")
+public class ItemDescController {
+    
+    @Autowired
+    private ItemDescService itemDescService;
+    
+    @RequestMapping(value="{itemId}",method=RequestMethod.GET)
+    public ResponseEntity<ItemDesc> queryItemDescByItemId(@PathVariable("itemId")Long itemId) {
+     
+        try {
+            
+            if(null==itemId){
+                return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+            }
+            ItemDesc itemDesc = this.itemDescService.queryById(itemId);
+            
+            if(null==itemDesc){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+            
+            return ResponseEntity.status(HttpStatus.OK).body(itemDesc);
+           
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        
+    }
+}
