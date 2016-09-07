2ea9be81e0bdc1e0e8a9ec4feaec2835ccf47cec
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-07 04:05:18 +0000

    Item的包装类，目的是为了在商品列表中显示商品类目名称，而不是cid。还需要mapper和mapper.xml以及修改Itemcontroller和ItemService的支持，而这些工作都待做
    git-svn-id: svn://localhost/23taotao@65 282828a6-f508-ba43-874f-5e0e018e640a

---
+++ src/wotao-manage/trunk/wotao-manage-web/src/main/java/com/taotao/manage/bean/Item_Vo_Cat.java
@@ -0,0 +1,17 @@
+package com.taotao.manage.bean;
+
+import com.taotao.manage.pojo.Item;
+
+public class Item_Vo_Cat extends Item {
+
+    private String item_Vo_name;
+
+    public String getItem_Vo_name() {
+        return item_Vo_name;
+    }
+
+    public void setItem_Vo_name(String item_Vo_name) {
+        this.item_Vo_name = item_Vo_name;
+    }
+    
+}
