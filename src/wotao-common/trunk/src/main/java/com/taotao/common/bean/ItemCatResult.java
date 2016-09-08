b38301838ae5b5b8e722ad409528ced036e4f0a0
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-08 09:52:22 +0000

    day03 用于封装ItemCatData对象（ItemCatData对象用于描述三级类目，每一个ItemCatData对象对应一个三级类目），即封装所有三级类目
    git-svn-id: svn://localhost/23taotao@112 282828a6-f508-ba43-874f-5e0e018e640a

---
+++ src/wotao-common/trunk/src/main/java/com/taotao/common/bean/ItemCatResult.java
@@ -0,0 +1,21 @@
+package com.taotao.common.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class ItemCatResult {
+
+	@JsonProperty("data")//把加注解的这个类转化为JSON格式并指定转化后的名称为"data"
+	private List<ItemCatData> itemCats = new ArrayList<ItemCatData>();
+
+	public List<ItemCatData> getItemCats() {
+		return itemCats;
+	}
+
+	public void setItemCats(List<ItemCatData> itemCats) {
+		this.itemCats = itemCats;
+	}
+
+}
