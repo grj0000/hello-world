cdd99dd0999f62dad36f1916182f99260b421027
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-08 08:03:42 +0000

    day03 	添加注释 @JsonProperty("data")//把加注解的这个类转化为JSON格式并指定转化后的名称为"data"
    git-svn-id: svn://localhost/23taotao@110 282828a6-f508-ba43-874f-5e0e018e640a

--- src/wotao-manage/trunk/wotao-manage-web/src/main/java/com/taotao/manage/bean/ItemCatResult.java
+++ src/wotao-manage/trunk/wotao-manage-web/src/main/java/com/taotao/manage/bean/ItemCatResult.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class ItemCatResult {
 
-	@JsonProperty("data")
+	@JsonProperty("data")//把加注解的这个类转化为JSON格式并指定转化后的名称为"data"
 	private List<ItemCatData> itemCats = new ArrayList<ItemCatData>();
 
 	public List<ItemCatData> getItemCats() {
