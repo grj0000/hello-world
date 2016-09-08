2bb7bd913b618394f31c6d62f2059fd8d7c039db
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-08 11:08:19 +0000

    day03 添加了参数callback，并根据callback的值是否为空返回不同的拼接 效果有了，但是是中文乱码 鼠标放在首页的全部商品分类下面任一分类，然后执行请求，然后显示了分类了，但是是乱码     public ResponseEntity<String> queryItemCat(@RequestParam("callback")String callback) {                 String json = MAPPER.writeValueAsString(itemCatResult);                 if(StringUtils.isEmpty(callback)){                     return ResponseEntity.status(HttpStatus.OK).body(json);                 }else{                     return ResponseEntity.status(HttpStatus.OK).body(callback+"("+json+")");                 }
    git-svn-id: svn://localhost/23taotao@119 282828a6-f508-ba43-874f-5e0e018e640a

--- src/wotao-manage/trunk/wotao-manage-web/src/main/java/com/taotao/manage/controller/api/ApiItemCatController.java
+++ src/wotao-manage/trunk/wotao-manage-web/src/main/java/com/taotao/manage/controller/api/ApiItemCatController.java
@@ -1,12 +1,15 @@
 package com.taotao.manage.controller.api;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taotao.common.bean.ItemCatResult;
 import com.taotao.manage.service.ItemCatService;
 
@@ -17,45 +20,45 @@ public class ApiItemCatController {
     @Autowired 
     private ItemCatService itemCatService;
     
-//    private static final ObjectMapper MAPPER = new ObjectMapper();
-    
-    @RequestMapping(method=RequestMethod.GET)
-    public ResponseEntity<ItemCatResult> queryItemCat() {
-        
-        try {
-            ItemCatResult itemCatResult =this.itemCatService.queryAllToTree();
-            if(null==itemCatResult){
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-            }
-            return ResponseEntity.ok(itemCatResult);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
-        
-    }
+    private static final ObjectMapper MAPPER = new ObjectMapper();
     
 //    @RequestMapping(method=RequestMethod.GET)
-//    public ResponseEntity<String> queryItemCat(@RequestParam("callback")String callback) {
+//    public ResponseEntity<ItemCatResult> queryItemCat() {
 //        
 //        try {
 //            ItemCatResult itemCatResult =this.itemCatService.queryAllToTree();
-//          
-//            if(null!=itemCatResult){
-//                String json = MAPPER.writeValueAsString(itemCatResult);
-//                if(StringUtils.isEmpty(callback)){
-//                    return ResponseEntity.status(HttpStatus.OK).body(json);
-//                }else{
-//                    return ResponseEntity.status(HttpStatus.OK).body(callback+"("+json+")");
-//                }
+//            if(null==itemCatResult){
+//                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 //            }
-//            
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-//            
+//            return ResponseEntity.ok(itemCatResult);
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
 //        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
 //        
 //    }
+    
+    @RequestMapping(method=RequestMethod.GET)
+    public ResponseEntity<String> queryItemCat(@RequestParam("callback")String callback) {
+        
+        try {
+            ItemCatResult itemCatResult =this.itemCatService.queryAllToTree();
+          
+            if(null!=itemCatResult){
+                String json = MAPPER.writeValueAsString(itemCatResult);
+                if(StringUtils.isEmpty(callback)){
+                    return ResponseEntity.status(HttpStatus.OK).body(json);
+                }else{
+                    return ResponseEntity.status(HttpStatus.OK).body(callback+"("+json+")");
+                }
+            }
+            
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        
+    }
 }
