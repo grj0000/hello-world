4e6ace97cce4ceec590797a4ed656f51146c819a
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-09 10:34:50 +0000

    day05 将商品类目信息存入缓存 1在从数据库获取数据之前，先从缓存取数据，为不影响正常逻辑，要加入缓存中取不到（就是未命中）数据的判断。 2在向前端返回数据之前，存入缓存，同样不能影响主逻辑。 3再次强调缓存不能影响主逻辑，就是如果缓存没有开启，程序照常运行，为达到这样的目的就要加判断，捕获异常 public ItemCatResult queryAllToTree() {  ItemCatResult result = new ItemCatResult();  try {             String cacheData = this.redisService.get(REDIS_KEY);
                if (StringUtils.isNotEmpty(cacheData)) {
    
                    return MAPPER.readValue(cacheData, ItemCatResult.class);
    
                }
            } catch (JsonParseException e) {……
    
     try {
                this.redisService.set(REDIS_KEY, MAPPER.writeValueAsString(result), REDIS_TIME);// 缓存原则，一般半年以上
            } catch (JsonProcessingException e) {
                e.printStackTrace();
            } catch (Exception e) {
                e.printStackTrace();
            }
            return result;
    
    git-svn-id: svn://localhost/23taotao@156 282828a6-f508-ba43-874f-5e0e018e640a

--- src/wotao-manage/trunk/wotao-manage-service/src/main/java/com/taotao/manage/service/ItemCatService.java
+++ src/wotao-manage/trunk/wotao-manage-service/src/main/java/com/taotao/manage/service/ItemCatService.java
@@ -1,16 +1,23 @@
 package com.taotao.manage.service;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taotao.common.bean.EasyUIResult;
 import com.taotao.common.bean.ItemCatData;
 import com.taotao.common.bean.ItemCatResult;
+import com.taotao.common.service.RedisService;
 import com.taotao.manage.mapper.ItemCatMapper;
 import com.taotao.manage.pojo.ItemCat;
 
@@ -20,53 +27,92 @@ public class ItemCatService extends BaseService<ItemCat> {
     @Autowired
     private ItemCatMapper itemCatMapper;
 
+    @Autowired
+    private RedisService redisService;
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    private static final String REDIS_KEY = "TAOTAO_MANAGE_ITEM_CAT_ALL";// 最佳实践：项目名_模块名_业务名
+
+    private static final Integer REDIS_TIME = 60 * 60 * 24 * 30 * 6;
+
     public ItemCatResult queryAllToTree() {
         ItemCatResult result = new ItemCatResult();
+
+        try {
+            String cacheData = this.redisService.get(REDIS_KEY);
+
+            if (StringUtils.isNotEmpty(cacheData)) {
+
+                return MAPPER.readValue(cacheData, ItemCatResult.class);
+
+            }
+        } catch (JsonParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         // 全部查出，并且在内存中生成树形结构
         List<ItemCat> cats = super.queryAll();
-        
+
         // 转为map存储，key为父节点ID，value为数据集合
         Map<Long, List<ItemCat>> itemCatMap = new HashMap<Long, List<ItemCat>>();
         for (ItemCat itemCat : cats) {
-                if(!itemCatMap.containsKey(itemCat.getParentId())){
-                        itemCatMap.put(itemCat.getParentId(), new ArrayList<ItemCat>());
-                }
-                itemCatMap.get(itemCat.getParentId()).add(itemCat);
+            if (!itemCatMap.containsKey(itemCat.getParentId())) {
+                itemCatMap.put(itemCat.getParentId(), new ArrayList<ItemCat>());
+            }
+            itemCatMap.get(itemCat.getParentId()).add(itemCat);
         }
-        
+
         // 封装一级对象
         List<ItemCat> itemCatList1 = itemCatMap.get(0L);
         for (ItemCat itemCat : itemCatList1) {
-                ItemCatData itemCatData = new ItemCatData();
-                itemCatData.setUrl("/products/" + itemCat.getId() + ".html");
-                itemCatData.setName("<a href='"+itemCatData.getUrl()+"'>"+itemCat.getName()+"</a>");
-                result.getItemCats().add(itemCatData);
-                if(!itemCat.getIsParent()){
-                        continue;
+            ItemCatData itemCatData = new ItemCatData();
+            itemCatData.setUrl("/products/" + itemCat.getId() + ".html");
+            itemCatData.setName("<a href='" + itemCatData.getUrl() + "'>" + itemCat.getName() + "</a>");
+            result.getItemCats().add(itemCatData);
+            if (!itemCat.getIsParent()) {
+                continue;
+            }
+
+            // 封装二级对象
+            List<ItemCat> itemCatList2 = itemCatMap.get(itemCat.getId());
+            List<ItemCatData> itemCatData2 = new ArrayList<ItemCatData>();
+            itemCatData.setItems(itemCatData2);
+            for (ItemCat itemCat2 : itemCatList2) {
+                ItemCatData id2 = new ItemCatData();
+                id2.setName(itemCat2.getName());
+                id2.setUrl("/products/" + itemCat2.getId() + ".html");
+                itemCatData2.add(id2);
+                if (itemCat2.getIsParent()) {
+                    // 封装三级对象
+                    List<ItemCat> itemCatList3 = itemCatMap.get(itemCat2.getId());
+                    List<String> itemCatData3 = new ArrayList<String>();
+                    id2.setItems(itemCatData3);
+                    for (ItemCat itemCat3 : itemCatList3) {
+                        itemCatData3.add("/products/" + itemCat3.getId() + ".html|" + itemCat3.getName());
+                    }
                 }
-                
-                // 封装二级对象
-                List<ItemCat> itemCatList2 = itemCatMap.get(itemCat.getId());
-                List<ItemCatData> itemCatData2 = new ArrayList<ItemCatData>();
-                itemCatData.setItems(itemCatData2);
-                for (ItemCat itemCat2 : itemCatList2) {
-                        ItemCatData id2 = new ItemCatData();
-                        id2.setName(itemCat2.getName());
-                        id2.setUrl("/products/" + itemCat2.getId() + ".html");
-                        itemCatData2.add(id2);
-                        if(itemCat2.getIsParent()){
-                                // 封装三级对象
-                                List<ItemCat> itemCatList3 = itemCatMap.get(itemCat2.getId());
-                                List<String> itemCatData3 = new ArrayList<String>();
-                                id2.setItems(itemCatData3);
-                                for (ItemCat itemCat3 : itemCatList3) {
-                                        itemCatData3.add("/products/" + itemCat3.getId() + ".html|"+itemCat3.getName());
-                                }
-                        }
-                }
-                if(result.getItemCats().size() >= 14){
-                        break;
-                }
+            }
+            if (result.getItemCats().size() >= 14) {
+                break;
+            }
+        }
+        try {
+            this.redisService.set(REDIS_KEY, MAPPER.writeValueAsString(result), REDIS_TIME);// 缓存原则，一般半年以上
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return result;
     }
@@ -76,6 +122,5 @@ public class ItemCatService extends BaseService<ItemCat> {
      * 
      * @return
      */
-    
-    
+
 }
