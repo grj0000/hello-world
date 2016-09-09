5304082d751da01c13d26444e281acb74d48a6ca
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-09 09:15:10 +0000

    day04 把大广告和小广告需要的字符串返回
    git-svn-id: svn://localhost/23taotao@149 282828a6-f508-ba43-874f-5e0e018e640a

--- src/wotao-web/trunk/src/main/java/com/taotao/web/controller/IndexController.java
+++ src/wotao-web/trunk/src/main/java/com/taotao/web/controller/IndexController.java
@@ -6,23 +6,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.taotao.web.service.IndexService;
+
 
 	
 @Controller
 public class IndexController {
 
-//	@Autowired
-//	private IndexService indexService;
+	@Autowired
+	private IndexService indexService;
 	
         @RequestMapping(value="index",method=RequestMethod.GET)
         public ModelAndView index() {
             
             ModelAndView mv = new ModelAndView("index");
-//            String indexAd1 = this.indexService.queryAD1();
-//            mv.addObject("indexAD1", indexAd1);
-//            
-//            String indexAd2 = this.indexService.queryAD2();
-//            mv.addObject("indexAD2", indexAd2);
+            String indexAd1 = this.indexService.queryAD1();
+            mv.addObject("indexAD1", indexAd1);
+            
+            String indexAd2 = this.indexService.queryAD2();
+            mv.addObject("indexAD2", indexAd2);
             
             return mv;
             
