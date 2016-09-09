994164882fa4797f2d68fdd878f9ed90d1721724
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-09 09:16:34 +0000

    day04 用于封装httpclient的相应 	private Integer code; 	private String data;
    git-svn-id: svn://localhost/23taotao@150 282828a6-f508-ba43-874f-5e0e018e640a

---
+++ src/wotao-common/trunk/src/main/java/com/taotao/common/httpclient/HttpResult.java
@@ -0,0 +1,29 @@
+package com.taotao.common.httpclient;
+
+public class HttpResult {
+	
+	private Integer code;
+	private String data;
+	
+	public HttpResult() {
+	}
+	
+	public HttpResult(Integer code, String data) {
+		this.code = code;
+		this.data = data;
+	}
+
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public Integer getCode() {
+		return code;
+	}
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+	
+}
