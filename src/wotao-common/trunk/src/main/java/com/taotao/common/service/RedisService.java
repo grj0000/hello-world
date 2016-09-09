5a4c1b952bc41060b22b63a5630ca7a716151692
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-09 10:40:01 +0000

    day05 封装了redis缓存的众多方法 public <T> T excute(Function<T, ShardedJedis> fun) { 		ShardedJedis shardedJedis = null; 		try { 			// 从连接池中获取到jedis分片对象 			shardedJedis = shardedJedisPool.getResource(); 			return fun.callback(shardedJedis); 		} catch (Exception e) { 			e.printStackTrace(); 		} finally { 			if (null != shardedJedis) { 				// 关闭，检测连接是否有效，有效则放回到连接池中，无效则重置状态 				shardedJedis.close(); 			} 		} 		return null; 	} public String set(final String key, final String value) { public String get(final String key) { public Long del(final String key) { public Long expire(final String key, final Integer seconds) { public String set(final String key, final String value, final Integer seconds) {
    git-svn-id: svn://localhost/23taotao@158 282828a6-f508-ba43-874f-5e0e018e640a

---
+++ src/wotao-common/trunk/src/main/java/com/taotao/common/service/RedisService.java
@@ -0,0 +1,83 @@
+package com.taotao.common.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
+
+@Service
+public class RedisService {
+
+	@Autowired(required = false)//如果spring容器中有就注入，没有就忽略
+	ShardedJedisPool shardedJedisPool;
+
+	public <T> T excute(Function<T, ShardedJedis> fun) {
+		ShardedJedis shardedJedis = null;
+		try {
+			// 从连接池中获取到jedis分片对象
+			shardedJedis = shardedJedisPool.getResource();
+			return fun.callback(shardedJedis);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (null != shardedJedis) {
+				// 关闭，检测连接是否有效，有效则放回到连接池中，无效则重置状态
+				shardedJedis.close();
+			}
+		}
+		return null;
+	}
+
+	public String set(final String key, final String value) {
+		return this.excute(new Function<String, ShardedJedis>() {
+
+			@Override
+			public String callback(ShardedJedis e) {
+				return e.set(key, value);
+			}
+		});
+	}
+
+	public String get(final String key) {
+		return this.excute(new Function<String, ShardedJedis>() {
+
+			@Override
+			public String callback(ShardedJedis e) {
+				return e.get(key);
+			}
+		});
+	}
+	
+	public Long del(final String key) {
+		return this.excute(new Function<Long, ShardedJedis>() {
+
+			@Override
+			public Long callback(ShardedJedis e) {
+				return e.del(key);
+			}
+		});
+	}
+	
+	public Long expire(final String key, final Integer seconds) {
+		return this.excute(new Function<Long, ShardedJedis>() {
+
+			@Override
+			public Long callback(ShardedJedis e) {
+				return e.expire(key, seconds);
+			}
+		});
+	}
+	
+	public String set(final String key, final String value, final Integer seconds) {
+		return this.excute(new Function<String, ShardedJedis>() {
+
+			@Override
+			public String callback(ShardedJedis e) {
+				String result = e.set(key, value);
+				e.expire(key, seconds);
+				return result;
+			}
+		});
+	}
+}
