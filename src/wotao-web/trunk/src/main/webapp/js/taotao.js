6e1e085926be4ebe47678d37045aea84f8f5988a
guorj <guorj@282828a6-f508-ba43-874f-5e0e018e640a>
2016-09-10 15:00:47 +0000

    day06 因为单点登录userController根据token查询用户信息直接返回的json数据，不需要再做对象转json的处理，所以做了相应改动
    git-svn-id: svn://localhost/23taotao@198 282828a6-f508-ba43-874f-5e0e018e640a

--- src/wotao-web/trunk/src/main/webapp/js/taotao.js
+++ src/wotao-web/trunk/src/main/webapp/js/taotao.js
@@ -9,8 +9,7 @@ var TT = TAOTAO = {
 			dataType : "jsonp",
 			type : "GET",
 			success : function(data){
-					var _data = JSON.parse(data);
-					var html =_data.username+"，欢迎来到淘淘！<a href=\"http://www.taotao.com/user/logout.html\" class=\"link-logout\">[退出]</a>";
+					var html =data.username+"，欢迎来到淘淘！<a href=\"http://www.taotao.com/user/logout.html\" class=\"link-logout\">[退出]</a>";
 					$("#loginbar").html(html);
 			}
 		});
